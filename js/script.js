24d90c6b9078847a0e426660de0ff58c6b22e766
KeshetBehanan <talisraeli225@gmail.com>
2021-03-31 20:30:25 +0300

    Added navbar and cover section

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// particles-js
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
