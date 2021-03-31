d2c2af5b986cab49c3027eb0fdcb743a47f63d41
KeshetBehanan <talisraeli225@gmail.com>
2021-03-31 21:30:38 +0300

    Added about section

--- js/script.js
+++ js/script.js
@@ -4,3 +4,23 @@
 particlesJS.load('particles-js', 'assets/particles.json', function() {
     console.log('callback - particles.js config loaded');
   });
+
+  // Typed.js
+
+  var typed = new Typed('#typed', {
+    strings: [
+      'a Unity Game Developer',
+      'a .NET Software Engineer',
+      'a Designer',
+      'a Full-Stack Web Developer',
+      'a Gamer',
+      'a Discord Bot Developer'
+    ],
+    smartBackspace: true,
+    shuffle: true,
+    loop: true,
+    backDelay: 2000,
+    backSpeed: 25,
+    typeSpeed: 50,
+    contentType: 'null',
+  });
