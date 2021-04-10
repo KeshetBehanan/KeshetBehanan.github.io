ebedcffe1cc0ce8de0c9b27c1ea90f09e47c375f
KeshetBehanan <talisraeli225@gmail.com>
2021-04-10 04:53:08 +0300

    preloading screen added

--- js/script.js
+++ js/script.js
@@ -30,3 +30,13 @@ var typed = new Typed('#typed', {
 // Mobile viewport hight
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// Loading
+window.onload = function(){
+  var loader = document.getElementById('loader');
+  document.body.parentElement.className = '';
+  loader.className += 'fade';
+  setTimeout(function(){
+    loader.parentElement.removeChild(loader);
+  }, 1000);
+}
